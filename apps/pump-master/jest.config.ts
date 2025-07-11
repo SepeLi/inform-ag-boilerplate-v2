62e5358b118d0930472fde964cfece7302c5dac3
Sean Li <15363540600@163.com>
2025-07-11 17:36:58 +1000

    Initial commit

---
+++ apps/pump-master/jest.config.ts
@@ -0,0 +1,19 @@
+import type { Config } from 'jest';
+import nextJest from 'next/jest.js';
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+const config: Config = {
+  displayName: '@inform-ag-boilerplate-v2/pump-master',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/pump-master',
+  testEnvironment: 'jsdom',
+};
+
+export default createJestConfig(config);
